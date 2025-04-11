8949ae4cb7867932078c0b2589061008732b3211
hammezii <hannmerry@gmail.com>
2025-04-11 21:45:53 +0100

    Update cleaner.py
    Updating code to make it more efficient. CSS elements are now taken as a list at the start of the code.

--- cleaner.py
+++ cleaner.py
@@ -3,51 +3,51 @@ import html2text
 import os
 from bs4 import BeautifulSoup
 
-# set directory for to_convert (drop files here to be converted) 
-# and to_save (where markdown files will be saved)
-to_convert = "to_convert"
-to_save = "markdown_output"
+# set directory for input_dir (drop files here to be converted) 
+# and output_dir (where markdown files will be saved)
+input_dir = "dndb html to md/Convert" 
+output_dir = "dndb html to md/Converted"
 
-# get list of files in to_convert folder
-files = os.listdir(to_convert)
+# List of CSS elements to remove
+elements_to_remove = [
+    '.ddb-site-banner',
+    '.b-noscript',
+    '.ddb-footer',
+    '.site-bar__container',
+    '.page-header',
+    '.p-article-byline',
+    '#mega-menu-target',
+    '#skip-to-content-link',
+    'header',
+    'footer'
+]
 
-# uses Beautiful Soup to strip out header, footer, navigation text
+# get list of html files in input_dir folder
+files = []
+for file in os.listdir(input_dir):
+    if file.endswith(('.html','htm')):
+        files.append(file)
+
+# uses Beautiful Soup to strip out elements in elements_to_remove
 #open html_file and reads it
 for file in files:  
-    with open(to_convert+'/'+file, 'r', encoding='utf-8', errors='ignore') as fff:
+    with open(os.path.join(input_dir, file), 'r', encoding='utf-8', errors='ignore') as fff:
         html_content = fff.read()
 
     # reads file with beautiful soup
     soup = BeautifulSoup(html_content, 'html.parser')
     soup.encode('UTF-8')
 
-    # create list of div class you want to remove from html document
-    classes = ['ddb-site-banner', 'b-noscript', 'ddb-footer', 'site-bar__container', 'page-header', 'p-article-byline']
-
-    # create list of div id you want to remove from html document
-    ids = ['mega-menu-target', 'skip-to-content-link']
-
-    # look through to remove classes
-    for div in classes:
-        element = soup.find(class_=div)
-        if element:
+    # remove elements
+    for element in elements_to_remove:
+        for element in soup.select(element):
             element.decompose()
-
-    # loop through to remove ids
-    for id in ids:
-        element = soup.find(id=id)
-        if element:
-            element.decompose()
-
-    # removes website header and footer
-    if soup.header:
-        soup.header.decompose()
-    if soup.footer:
-        soup.footer.decompose()
 
     # overwrite the file with the new cleaned version
-    with open(to_convert+'/'+file, 'w', encoding='utf-8') as ffff: #overwrite the file
+    with open(os.path.join(input_dir, file), 'w', encoding='utf-8') as ffff: #overwrite the file
         ffff.write(str(soup))
+
+    print(file+" is cleaned.")
 
 # now html files are cleaned, runs them through html2text to convert them to markdown
 # set html2text function and options: see config for all options
@@ -56,10 +56,10 @@ cleaner.ignore_images = True
 cleaner.ignore_links = True
 cleaner.body_width = 0
 
-# Conversion loop: performs conversion for each file in to_convert
+# Conversion loop: performs conversion for each file in input_dir
 # opens and reads each file from files
 for file in files:
-    with open(to_convert+'/'+file, "r", encoding='UTF-8', errors='ignore') as f:
+    with open(os.path.join(input_dir, file), "r", encoding='UTF-8', errors='ignore') as f:
         html = f.read()
 
     # change unicode characters to single or double quotation mark to be read by markdown
@@ -69,18 +69,20 @@ for file in files:
     text = cleaner.handle(tidy_html)
 
     # Open a file and write the conversion to it
-    with open(to_save+'/'+file, 'w') as ff:
+    with open(os.path.join(output_dir, file), 'w') as ff:
         ff.write(text)   
 
-# Rename converted files in to_save folder
-# get list of files in to_save folder
-converted = os.listdir(to_save)
+    print(file+" is converted to markdown.")
+
+# Rename converted files in output_dir folder
+# get list of files in output_dir folder
+converted = os.listdir(output_dir)
 
 # rename each converted files
 for convert in converted:
     newfiles = []
     newfiles.append(os.path.splitext(convert)[0]+'.md')
     for newfile in newfiles:
-        os.replace(to_save+'/'+convert, to_save+'/'+newfile)
+        os.replace(os.path.join(output_dir, convert), os.path.join(output_dir, newfile))
 
 print("File conversion complete!")
